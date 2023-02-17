9ad4596bdda808815eb8c48d318ab3b39e2af0dc
Irving Wang <irvingywang@gmail.com>
2023-02-16 20:58:19 -0600

    resolve merge conflicts

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -170,7 +170,6 @@ public final class Constants {
   public static final class NeoMotorConstants {
     public static final double kFreeSpeedRpm = 5676;
   }
-}
 
   public static final class ArmConstants {
     //Spark IDs
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -182,3 +182,8 @@ public class RobotContainer {
     // Run path following command, then stop at the end.
     return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false, false));
   }
+
+  public Command getNothingAuto() {
+    return new NothingAuto();
+  }
+}
