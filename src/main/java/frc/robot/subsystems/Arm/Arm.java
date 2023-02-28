3ef06c1b3557ea12d89db1b050aad8bafe4fc895
Irving Wang <irvingywang@gmail.com>
2023-02-28 00:59:22 -0600

    fix leds

--- src/main/java/frc/robot/subsystems/Arm/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm/Arm.java
@@ -8,7 +8,6 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
